b3b345a6856109034d9d0bb4694e8cc64b6ad258
Ruby <ruby@Ubuntu.myguest.virtualbox.org>
2023-09-07 19:33:00 -0400

    functions done git and env left

--- .zshrc
+++ .zshrc
@@ -5,6 +5,8 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="robbyrussell"
 plugins=(git zsh-autosuggestions dirhistory docker)
 
+PS1='[%*] %M@%d';
+
 source $ZSH/oh-my-zsh.sh
 source ./.aliases
 source ./.functions
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ mkdir ~/bin ~/cegep ~/projet;
 
 
 sudo apt install git neovim gcc gdb python3 python3-pip python3-venv;
+ git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim;
 
 cp -r .zshrc ~/
 cp -r .aliases ~/
